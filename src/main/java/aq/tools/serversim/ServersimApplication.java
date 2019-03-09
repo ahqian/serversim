abc83e99d99114ddeb4b79da0f1acc1ca98e0dc2
Alex Qian <alex.bqian@gmail.com>
2019-03-09 15:51:14 -0500

    initial commit

---
+++ src/main/java/aq/tools/serversim/ServersimApplication.java
@@ -0,0 +1,13 @@
+package aq.tools.serversim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersimApplication.class, args);
+	}
+
+}
